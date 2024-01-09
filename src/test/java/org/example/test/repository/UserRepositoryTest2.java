c8dc5a286c4d8b8d7c78a6769f9a64c993d7b584
CNCITY <thomas.sang@cncity.co.kr>
2024-01-09 12:42:08 +0900

    JPA > MyBatis : Test Code
    User를 생성 > 유저 추가
    DB에 값 들어가는거 확인완료

---
+++ src/test/java/org/example/test/repository/UserRepositoryTest2.java
@@ -0,0 +1,26 @@
+package org.example.test.repository;
+
+import org.example.test.entity.User;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class UserRepositoryTest2 {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    void test() {
+        User user = User.builder()
+                        .email("test@naver.com")
+                                .username("Thomas")
+                                        .build();
+
+        userRepository.getUserMapper().insertUser(user);
+    }
+
+}
