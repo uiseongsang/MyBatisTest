062a79d81c1e3bdb4b7a3f3f75df75f63079f1bb
CNCITY <thomas.sang@cncity.co.kr>
2024-01-09 12:38:07 +0900

    JPA > MyBatis : Controller post /user만 사용!

--- src/main/java/org/example/test/controller/UserController.java
+++ src/main/java/org/example/test/controller/UserController.java
@@ -1,46 +1,46 @@
 package org.example.test.controller;
 
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.example.test.dto.*;
 import org.example.test.entity.User;
 import org.example.test.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
-
-    public UserController(UserService userService) {
-        this.userService = userService;
-    }
 
     @PostMapping("user")
     public ResponseEntity addUser(@RequestBody UserRequestDto requestDto){
         try {
                 UserResponseDto res = userService.addUser(requestDto);
-                return ResponseEntity.status(201).body(res);
+                return ResponseEntity.status(HttpStatus.CREATED).body(res);
             } catch (IllegalArgumentException e) {
-                return ResponseEntity.status(500).body("error");
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Email has been Duplicated");
             }
     }
 
-    @GetMapping("users")
-    public UserListResponseDto selectUsers(){
-        return userService.selectUsers();
-    }
+//    @GetMapping("users")
+//    public UserListResponseDto selectUsers(){
+//        return userService.selectUsers();
+//    }
 
-    @PostMapping("board/userId/{userId}")
-    public ResponseEntity addUserBoard(@RequestBody BoardRequestDto requestDto, @PathVariable Long userId) {
-        BoardResponseDto res = userService.addUserBoard(requestDto,userId);
-
-        return ResponseEntity.status(HttpStatus.CREATED).body(res);
-    }
-
-    @GetMapping("board/userId/{userId}")
-    public BoardListResponseDto selectUserBoards(@PathVariable Long userId) {
-        return userService.selectUserBoards(userId);
-    }
+//    @PostMapping("board/userId/{userId}")
+//    public ResponseEntity addUserBoard(@RequestBody BoardRequestDto requestDto, @PathVariable Long userId) {
+//        BoardResponseDto res = userService.addUserBoard(requestDto,userId);
+//
+//        return ResponseEntity.status(HttpStatus.CREATED).body(res);
+//    }
+//
+//    @GetMapping("board/userId/{userId}")
+//    public BoardListResponseDto selectUserBoards(@PathVariable Long userId) {
+//        return userService.selectUserBoards(userId);
+//    }
 
 }
