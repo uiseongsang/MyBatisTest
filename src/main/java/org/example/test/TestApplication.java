ceb8d547dd71f1f7b564da4bbffe0e11601fd194
CNCITY <thomas.sang@cncity.co.kr>
2024-01-09 12:31:21 +0900

    Refactoring: JPA 관련 어노테이션 삭제

--- src/main/java/org/example/test/TestApplication.java
+++ src/main/java/org/example/test/TestApplication.java
@@ -4,10 +4,8 @@ import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@EnableJpaAuditing
 public class TestApplication {
     @Bean
     public ModelMapper modelMapper() {
